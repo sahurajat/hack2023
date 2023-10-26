79750c81664e30d59094ba1ee7c13e7e0a8bc7e8
Rajat Sahu <119427607+sahurajat@users.noreply.github.com>
2023-10-26 12:05:46 -0400

    Create slididngwindow.java

---
+++ slididngwindow.java
@@ -0,0 +1,63 @@
+//wap to solve the SLIDING WINDOW maximum soln
+
+//TC_>O(nlogk)
+import java.util.*;
+public class nine
+{
+    static class Pair implements Comparable<Pair>
+    {
+        int val;
+        int idx;
+
+        public Pair(int val,int idx)
+        {
+            this.val=val;
+            this.idx=idx;
+        }
+
+        @Override
+        public int compareTo(Pair p2)
+        {
+            //ascending order sorting 
+            //return this.val-p2.val;
+
+            //for descending order
+            return p2.val-this.val;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,3,-1,-3,5,3,6,7};
+        int k=3;//window sie
+        int res[]=new int [arr.length-k+1];//n-k+1
+
+        PriorityQueue<Pair>pq=new PriorityQueue<>();
+
+        //1 st window
+        for(int i=0;i<k;i++)
+        {
+            pq.add(new Pair(arr[i], i));
+        }
+
+        res[0]=pq.peek().val;
+
+        for(int i=k;i<arr.length;i++)
+        {
+            while(pq.size() > 0 && pq.peek().idx <=(i-k) )
+            {
+                pq.remove();
+            }
+
+            pq.add(new Pair(arr[i], i));
+
+            //store all window result
+            res[i-k+1]=pq.peek().val;
+        }
+       //print the result
+       for(int i=0;i<res.length;i++)
+       {
+        System.out.print(res[i]+" ");
+       }
+       System.out.println();
+
+    }
+}
